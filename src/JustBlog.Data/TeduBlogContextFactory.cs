e044d0f2012f0cfbdc21ca3c18d7619e1a7346ee
Quang Duy <justinquangduy@gmail.com>
2023-12-25 17:30:54 +0700

    Migrate database

---
+++ src/JustBlog.Data/TeduBlogContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace JustBlog.Data
+{
+    public class TeduBlogContextFactory : IDesignTimeDbContextFactory<JustBlogContext>
+    {
+        public JustBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<JustBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new JustBlogContext(builder.Options);
+        }
+    }
+}
