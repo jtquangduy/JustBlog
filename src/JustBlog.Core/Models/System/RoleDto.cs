7b9d1b3093322050ade55f19666b59719b63bc15
Quang Duy <justinquangduy@gmail.com>
2024-01-03 17:05:24 +0700

    Role management for backend

---
+++ src/JustBlog.Core/Models/System/RoleDto.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using JustBlog.Core.Domain.Identity;
+
+namespace JustBlog.Core.Models.System
+{
+    public class RoleDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        public class AutoMapperProfiles : Profile
+        {
+            public AutoMapperProfiles()
+            {
+                CreateMap<AppRole, RoleDto>();
+            }
+        }
+    }
+}
